c9c99eda81139d55248a865c33fad4acbf63489d
CodeSandbox <support@codesandbox.io>
2023-11-23 09:07:36 +0000

    Initial commit

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,5 @@
 export function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <main className="container">
+      <h1 className="text-3xl font-bold">Title</h1>
+    </main>;
 }
